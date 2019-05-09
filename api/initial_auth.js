9c2a0eec7ca3d602263365b0b5ad3513433fa990
Josh Collinsworth <joshuajcollinsworth@gmail.com>
2019-05-09 16:44:22 -0500

    more testing

--- api/initial_auth.js
+++ api/initial_auth.js
@@ -8,32 +8,33 @@ module.exports = (req, res) => {
     let authCode = url.parse(req.url, true).query.auth_code;
     let body = encodeURI(`grant_type=authorization_code&client_id=${process.env.CLIENT_ID}&client_secret=${process.env.CLIENT_SECRET}&code=${authCode}`);
 
-    let args = {
-        hostname: 'getflywheel.namely.com',
-        path: '/api/v1/oauth2/token',
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/x-www-form-urlencoded'
-        }
-    }
+    res.end(JSON.stringify(body));
+    // let args = {
+    //     hostname: 'getflywheel.namely.com',
+    //     path: '/api/v1/oauth2/token',
+    //     method: 'POST',
+    //     headers: {
+    //         'Content-Type': 'application/x-www-form-urlencoded'
+    //     }
+    // }
 
-    const call = https.request(args, (resp) => {
-        let data = '';
+    // const call = https.request(args, (resp) => {
+    //     let data = '';
 
-        resp.on('data', (chunk) => {
-            data += chunk;
-        });
+    //     resp.on('data', (chunk) => {
+    //         data += chunk;
+    //     });
 
-        resp.on('end', () => {
-            res.end(JSON.stringify(data));
-        });
+    //     resp.on('end', () => {
+    //         res.end(JSON.stringify(data));
+    //     });
 
-    })
+    // })
 
-    call.on("error", (err) => {
-        res.end(JSON.stringify(err.message));
-    });
+    // call.on("error", (err) => {
+    //     res.end(JSON.stringify(err.message));
+    // });
 
-    call.write(body);
-    call.end();
+    // call.write(body);
+    // call.end();
 }
